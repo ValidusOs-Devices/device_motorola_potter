6a3b8a9dc10b20dd954c78d8c8f7cd197c3090fb
GtrCraft <nickvanbokhorst@hotmail.com>
2018-10-22 09:35:56 +0200

    potter: GZOSP -> Validus

--- validus_potter.mk
+++ validus_potter.mk
@@ -22,8 +22,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n.mk)
 # Inherit from potter device
 $(call inherit-product, device/motorola/potter/device.mk)
 
-# Inherit some common GZOSP stuff.
-$(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
+# Inherit some common Validus stuff.
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
@@ -31,7 +31,7 @@ TARGET_SCREEN_HEIGHT := 1920
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := potter
-PRODUCT_NAME := gzosp_potter
+PRODUCT_NAME := validus_potter
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 
